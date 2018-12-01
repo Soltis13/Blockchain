023b7dc8fce4aedab59f6eeedbccd63e445098f2
Scott <Soltis13@gmail.com>
2018-11-30 18:33:12 -0600

    blockchain update

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,3 +1,6 @@
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
+
 class Block {
     constructor({ timestamp, lastHash, hash, data}) {
         this.timestamp = timestamp;
@@ -5,6 +8,22 @@ class Block {
         this.hash = hash;
         this.data = data;
 
+    }
+
+    static genesis() {
+        return new this(GENESIS_DATA);
+    }
+
+    static mineBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
+        return new this({
+            timestamp,
+            lastHash,
+            data,
+            hash: cryptoHash(timestamp, lastHash, data)
+        });
     }
 }
 
